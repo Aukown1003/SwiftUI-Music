f5e0a8adcef8e129845020f6ee5eb3ae75daee98
Auk1003 <Aukown1003@users.noreply.github.com>
2023-03-13 18:03:53 +0900

    Initial Commit

---
+++ SwiftUI-Music/SwiftUI_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MusicApp.swift
+//  SwiftUI-Music
+//
+//  Created by 桑野友輔 on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
